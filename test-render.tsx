d7703b5142c592dac72aa111e78956143ed6c0ff
Okoro Godwin Chinedu <50265356+G0dgood@users.noreply.github.com>
2026-07-23 21:47:16 +0100

    refactor: remove redundant test utility files

--- test-render.tsx
+++
@@ -1,5 +0,0 @@
-import { renderToString } from "react-dom/server";
-import { Dropdown } from "./components/ui/dropdown";
-import * as React from "react";
-
-console.log(renderToString(<Dropdown label="All Offices" options={["London"]} />));
--- test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { Dropdown } from './components/ui/dropdown';
-
-console.log(renderToString(<Dropdown label="Test" options={["1", "2"]} />));
